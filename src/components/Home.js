18cf683f19a2750f91b269125175d9cd81e80878
Shubhanshu Singh <shubhanshusingh1796@gmail.com>
2022-08-18 11:04:31 +0530

    Initial commit

---
+++ src/components/Home.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import Product from "./Product";
+
+function Home() {
+  const [data, setData] = useState([]);
+
+  const url = "https://assessment.api.vweb.app/products";
+  const fetchData = async () => {
+    const response = await fetch(url);
+    const resData = await response.json();
+    setData(resData);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  return (
+    <>
+      <div className="flex flex-wrap w- text-center">
+        {data.map(({ product_id, name, stock, sellin_price }) => {
+          return (
+            <Product
+              product_id={product_id}
+              name={name}
+              stock={stock}
+              sellin_price={sellin_price}
+            />
+          );
+        })}
+      </div>
+    </>
+  );
+}
+
+export default Home;
